0e81c097b9d743afbe51fa464a79c304359145c5
kamimi01 <awakaru0122@gmail.com>
2020-03-08 15:22:17 +0900

    add: ロード時の初期化メソッドの実装

--- js/home.js
+++ js/home.js
@@ -1,1 +1,32 @@
 console.log("Hello World");
+
+(function() {
+  "use strict";
+
+  // 双方向データバインディング
+  var vm = new Vue({
+    el: "#app",
+    data: {
+      payload: {
+        user_id: "",
+        user_name: "",
+        category_list: [],
+        category_name: "",
+        product_list: [],
+        product_name: "",
+        product_price: 0,
+        product_img: ""
+      }
+    },
+    mounted: function() {
+      // データの初期化
+      this.initPage();
+    },
+    methods: {
+      initPage: function() {
+        this.payload.category_name = "どうぞ";
+        // APIからデータを取得して、商品情報を表示させたい
+      }
+    }
+  });
+})();
